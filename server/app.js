b5308ec713f5bab285b2244b25ea2118df1cc119
idankri <58448747+idankri@users.noreply.github.com>
2021-03-05 11:21:07 +0200

    Server side: added documentation and debugged

--- server/app.js
+++ server/app.js
@@ -66,48 +66,61 @@ const send_notification = (token, zone) => {
  */
 app.post('/login', (req, res) => {
     // verify email is not empty
-    if(req.body.email || req.body.email == ""){
-        return res.json({emailExists: false, passwordMatches: false});
+    if(!req.body.email || req.body.email == ""){
+        return res.status(500).json({emailExists: false, passwordMatches: false});
     }
+    // find if input email exists
     User.findOne({email: req.body.email}, (err, user) => {
         if(err) {
-            return res.json({message: err});
+            console.log(err);
+            return res.status(500).json({emailExists: false, passwordMatches: false, error: err});
         }
         console.log(user);
         if(!user){
-            return res.json({emailExists: false, passwordMatches: false});
-        }   
+            return res.status(200).json({emailExists: false, passwordMatches: false});
+        }
+        // in case user is found hash input password and compare
         bcrypt.compare(req.body.password, user.password, (err, result) => {
             if(err){
                 console.log(err);
-                return res.json({message: "123"});
+                return res.status(500).json({emailExists: true, passwordMatches: false, error: err});
             }
             if(result === false){
-                return res.json({emailExists: true, passwordMatches: false});
+                return res.status(200).json({emailExists: true, passwordMatches: false});
             }
-            if(req.body.token && req.body.longitude && req.body.latitude){
-                User.findOneAndUpdate({email: req.body.email}, 
-                    {token: req.body.token, 
-                    latitude: req.body.latitude, 
-                    longitude: req.body.longitude},
-                    (err, docs) => {
-                        if(err){
-                            console.log(err);
-                        }
-                        else{
-                            console.log(docs);
-                        }
-                    });
+            console.log(req.body.token);
+            if(req.body.token == undefined || req.body.longitude == undefined || req.body.latitude == undefined){
+                return res.status(500).json({emailExists: true, passwordMatches: false, error: true});
             }
-            
-            return res.json({emailExists: true, passwordMatches: true});
+            // if password matches and all parameters are filled update token and location
+            User.findOneAndUpdate({email: req.body.email}, 
+                {token: req.body.token, 
+                latitude: req.body.latitude, 
+                longitude: req.body.longitude},
+                (err, docs) => {
+                    if(err){
+                        console.log(err);
+                        return res.status(500).json({emailExists: true, passwordMatches: true, error:true});
+                    }
+                    else{
+                        console.log(docs);
+                    }
+                }
+            );
+            return res.status(200).json({emailExists: true, passwordMatches: true});
         });
     });
 });
 
+/**
+ * Express route for user registration.
+ * Body parameters: 
+ * email (string), 
+ * password (string), 
+ */
 app.post('/register', (req, res) => {
+    // hash the input password syncronously
     let hashed_pass = bcrypt.hashSync(req.body.password, 10);
-    
     const user = new User({
         email: req.body.email,
         password: hashed_pass
@@ -117,13 +130,17 @@ app.post('/register', (req, res) => {
         console.log(result);
     })
     .catch(err => {
-        console.log(err);
+        res.status(500).json({status: "Fail"});
     });
-    res.status(200).json({
-        status: "Success"
-    });
+    res.status(200).json({status: "Success"});
 });
 
+/**
+ * Express route for getting list of all zones.
+ * Responds with a message containing list of all zones, as a json
+ * object that contains zone id, zone name, and zone location (lat + lang)
+ * No parameters are needed for this request
+ */
 app.get('/zones', (req, res) => {
     let found_zones = []
     Zone.find((err, zones) => {
@@ -133,10 +150,20 @@ app.get('/zones', (req, res) => {
                 longitude: zone.longitude,
                 name: zone.name});
         })
-        res.json(found_zones);
+        res.json({"markers": found_zones});
     });
 });
 
+/**
+ * Express route for posting new zone
+ * Body parameters: 
+ * _id (string), 
+ * latitude (string), 
+ * longitude (number),
+ * latitude (number),
+ * name (string),
+ * description (string)
+ */
 app.post('/zones', (req, res) => {
     console.log(req.body._id);
     const zone = new Zone({
@@ -156,38 +183,81 @@ app.post('/zones', (req, res) => {
     
 });
 
+/**
+ * Express route for getting specifig details of certain zone
+ * Request parameters:
+ * id (number)
+ * Body parameters: 
+ * None
+ */
 app.get('/zone/:id', (req, res) => {
     Zone.findOne({_id: req.params.id}, (err, zone) => {
         if(err){
-            res.json({status: "fail"});
+            res.status(500).json({status: "fail"});
         }
-        res.json(zone);
+        res.status(200).json(zone);
     });
 })
 
+/**
+ * Express route for posting a review for a zone
+ * Request parameters:
+ * id (number) (id for zone)
+ * Body parameters: 
+ * foodRating (number), 
+ * crowdedRating (number), 
+ * priceRating (number),
+ * review (string)
+ */
 app.post('/zone/:id', (req, res) => {
+    if(req.body.foodRating < 0 || req.body.foodRating > 5){
+        res.status(500).json({status: "Fail"});
+    }
+    if(req.body.crowdedRating < 0 || req.body.crowdedRating > 5){
+        res.status(500).json({status: "Fail"});
+    }
+    if(req.body.priceRating < 0 || req.body.priceRating > 5){
+        res.status(500).json({status: "Fail"});
+    }
     Zone.findOne({_id: req.params.id}, (err, zone) => {
         if(err){
-            res.json({status: "fail"});
+            res.status(500).json({status: "Fail"});
         }
         //calculation of running average
         zone.foodRating = ((zone.foodRating * zone.numRatings) + req.body.foodRating)/(zone.numRatings + 1);
         zone.crowdedRating = ((zone.crowdedRating * zone.numRatings) + req.body.crowdedRating)/(zone.numRatings + 1);
         zone.priceRating = ((zone.priceRating * zone.numRatings) + req.body.priceRating)/(zone.numRatings + 1);
-        zone.reviews.push({"content": req.body.review});
+        if(req.body.review && req.body.review != ""){
+            zone.reviews.push({"content": req.body.review});
+        }
         zone.numRatings += 1;
-        console.log(zone.foodRating);
-        console.log(zone.numRatings);
         zone.save();
-        res.json({success: true});
+        res.status(200).json({status: "Success"});
     });
 });
 
+
+/**
+ * Express route for user checkin - notifying all users in the area that
+ * a user checked in a close zone (except of the user checking in)
+ * Body parameters: 
+ * id (number) (for zone)
+ * email (string)
+ */
 app.post('/checkin', async (req, res) => {
+    if(!req.body.id){
+        res.status(500).json({status: "Fail"});
+    }
     const email = req.body.email;
+    // find zone syncronously and verify it is a valid zone
     const zone = await Zone.findOne({_id: req.body.id});
+    console.log(zone);
+    if(!zone){
+        res.status(500).json({status: "Fail"});
+    }
     const latitude = zone.latitude;
     const longitude = zone.longitude;
+    // find all users that were last seen in the zone's area
     User.find(
         {email: {$ne: email}, 
         longitude: {$lte: longitude + 1, $gte: longitude - 1},
@@ -203,7 +273,7 @@ app.post('/checkin', async (req, res) => {
                 }
             });
         });
-        res.json({success: true});
+        res.status(200).json({status: "Success"});
 });
 
 app.listen(3000, () => {
